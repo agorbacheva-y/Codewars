408b40cc94779097ff76fc41b435debf44c04edc
agorbacheva-y <agorbacheva.y@gmail.com>
2023-08-26 22:34:42 +0200

    solution 1

---
+++ 8kyu/evenOrOdd.js
@@ -0,0 +1,11 @@
+// Create a function that takes an integer as an 
+// argument and returns "Even" for even numbers 
+// or "Odd" for odd numbers.
+
+function isEven(int) {
+  if (int % 2 == 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+};
